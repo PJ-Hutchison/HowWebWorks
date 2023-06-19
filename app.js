5b0c94b4f63774ecb5b540f4be198627fe76434b
PJ-Hutchison <100161458+PJ-Hutchison@users.noreply.github.com>
2023-06-19 12:14:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// // 1. What is HTTP?
+// Hyper Text Transfer Protocol, a coding method to transfer page data from a server across a network to a client
+// // 2. What is a URL?
+// Uniform Resource localStorage, address for an internet resource
+// // 3. What is DNS?
+// Domain Name FileSystem, turns URLS into IP Addresses
+// // 4. What is a query string?
+// Part of a URL that assigns values to parameters
+// // 5. What are two HTTP verbs and how are they different?
+// POST sends datat to the server, GET retrieves data from the server
+// // 6. What is an HTTP request?
+// It is a request from a client to a server following HTTP protocol
+// // 7. What is an HTTP response?
+// It is a response from a server to a client following HTTP protocol
+// // 8. What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// It's a response that passes additional context and data about requests or responses
+// Request headers: Host, User-Agent, Accept, Cookie, Cache-Control
+// Response headers: Content-Type, Last-Modified, Set-Cookie, Cache-Control
+// // 9. What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// The browser looks up an IP address for the domain 
+// The browser intiates TCP connecttion with a server
+// The browser sends a request to the server
+// The server processes the request and responds back
+// The browser renders the content of the page
